a2e99c86af5e292aa2c2ee459dfc4722c743dbd2
Andersson <a.cardenas.quiros@gmail.com>
2023-10-13 00:19:15 -0500

    Creación home completada

--- pm25-medellin/src/app/components/navbar/navbar.component.ts
+++ pm25-medellin/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,HostListener,ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  scrollYPosition:number=0;
+  bandera:boolean=false;
+  divStyles: any = {};
+  
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  @HostListener("window:scroll", ['$event'])
+  doSomethingOnWindowsScroll(event:Event):void{
+    this.scrollYPosition = window.scrollY;
+    if (this.scrollYPosition > 1 && this.bandera!=true) {
+      this.bandera = true;
+      this.divStyles = {
+        'transition': 'all 0.2s ease-in-out',
+        'box-shadow': '0 0px 10px black',
+      };
+    } else if(this.scrollYPosition==0) {
+      if (this.bandera) {
+        this.bandera = false;
+        this.divStyles = {};
+      }
+    }
+  }
 }
